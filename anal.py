124e3566dffb62a8f87c18ed36bba1befc3bf5c0
Ritam Saha <ritamsaha2005@gmail.com>
2024-11-17 01:53:25 -0800

    LINTED

--- anal.py
+++ anal.py
@@ -6,7 +6,7 @@ import sys
 
 data_list = glob(f"{sys.argv[1]}/jsons/*")
 
-pattern = re.compile(r'\-?\d+\.\d+|\-?\d+')
+pattern = re.compile(r"\-?\d+\.\d+|\-?\d+")
 
 right = 0
 all_fail = 0
@@ -19,24 +19,33 @@ from run_with_earlystopping import check
 retry_times = []
 retry_times_no_one = []
 
-def checklist(gt,anss):
+
+def checklist(gt, anss):
     for ans in anss:
-        if check(gt,ans):
+        if check(gt, ans):
             return True
     return False
 
+
 for file in data_list:
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         data = json.load(f)
-        retry_time = len(data['answers_list'])
+        retry_time = len(data["answers_list"])
         retry_times.append(retry_time)
         answers = []
-        for i in data['answers_list']:
-            if i in ["I Don't Know","I can't understand this question.","I can't help with this question.","I don't know how to solve this question.","I don't know the answer to this question.","I don't know the answer to this question, sorry."]:
+        for i in data["answers_list"]:
+            if i in [
+                "I Don't Know",
+                "I can't understand this question.",
+                "I can't help with this question.",
+                "I don't know how to solve this question.",
+                "I don't know the answer to this question.",
+                "I don't know the answer to this question, sorry.",
+            ]:
                 pass
             else:
                 answers.append(i)
-        if checklist(data['ground_truth'],answers):
+        if checklist(data["ground_truth"], answers):
             right += 1
         else:
             all_fail += 1
@@ -46,4 +55,4 @@ import numpy as np
 
 
 all_len = len(data_list)
-print('Acc rate',right,all_len,right/all_len)
+print("Acc rate", right, all_len, right / all_len)
--- ends_all_server.py
+++ ends_all_server.py
@@ -1,17 +1,18 @@
 import os
-table = os.popen('squeue').read()
-for line in str(table).split('\n'):
+
+table = os.popen("squeue").read()
+for line in str(table).split("\n"):
     # print(line)
-    if 'server.b' in line or 'batch.sh' in line:
-        id = line.split('vip_gpu')[0] #change to your slurm partition name
-        os.system(f'scancel {id}')
+    if "server.b" in line or "batch.sh" in line:
+        id = line.split("vip_gpu")[0]  # change to your slurm partition name
+        os.system(f"scancel {id}")
 # print(table)
 try:
-    os.remove('./server.csv')
+    os.remove("./server.csv")
 except:
     pass
 try:
-    os.system('rm *.out')
+    os.system("rm *.out")
 except:
     pass
-os.system('squeue')
+os.system("squeue")
